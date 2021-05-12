3bb43a9a3e6c79b64022db026a89c2e702d3d8ca
Digo-Castro <digo_castro@outlook.com>
2021-05-12 20:39:32 -0300

    [Desafio 6]  - OK

---
+++ challenges/desafio6.js
@@ -0,0 +1,17 @@
+db.movies.aggregate([
+  { $match: { $and: [{ awards: /Won/ }, { awards: /Oscar/ }] } },
+  { $group: {
+    _id: null,
+    maior: { $max: "$imdb.rating" },
+    menor: { $min: "$imdb.rating" },
+    media: { $avg: "$imdb.rating" },
+    desvio: { $stdDevSamp: "$imdb.rating" },
+  } },
+  { $project: {
+    _id: 0,
+    maior_rating: "$maior",
+    menor_rating: "$menor",
+    media_rating: { $round: ["$media", 1] },
+    desvio_padrao: { $round: ["$desvio", 1] },
+  } },
+]);
