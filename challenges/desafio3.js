74bca37c5c1f7f20433ab41739d324ddc17c9f6b
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-05-12 17:13:21 -0300

    Challenge 3 done

---
+++ challenges/desafio3.js
@@ -0,0 +1,31 @@
+// How to optimize a pipeline sequence(using $match, $sort and much more):
+// https://docs.mongodb.com/manual/core/aggregation-pipeline-optimization/
+
+db.movies.aggregate(
+  [{
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
+  },
+  {
+    $sort: {
+      year: -1,
+      "imdb.rating": -1,
+      title: 1,
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+  ],
+);
