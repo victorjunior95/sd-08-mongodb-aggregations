184c0941b6a297412ff2480f4f95c8d243e417c9
Leonardo <lecastro16@gmail.com>
2021-05-12 17:56:41 -0300

    requisito 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,21 @@
+db.movies.aggregate([
+  { $match: { awards: { $regex: /^won/i } } },
+  {
+    $group: {
+      _id: null,
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media: { $avg: "$imdb.rating" },
+      desvio: { $stdDevSamp: "$imdb.rating" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maior_rating: 1,
+      menor_rating: 1,
+      media_rating: { $round: ["$media", 1] },
+      desvio_rating: { $round: ["$desvio", 1] },
+    },
+  },
+]);
