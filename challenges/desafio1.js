db29442155fd5b903cd6d3198b099404ce285b49
Leonardo <lecastro16@gmail.com>
2021-05-12 14:39:35 -0300

    requisito 1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,12 @@
-// Leonardo Sardinha
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        { "imdb.rating": { $gte: 7 } },
+        { genres: { $nin: ["Crime", "Horror"] } },
+        { rated: { $in: ["PG", "G"] } },
+        { languages: { $all: ["English", "Spanish"] } },
+      ],
+    },
+  },
+]);
