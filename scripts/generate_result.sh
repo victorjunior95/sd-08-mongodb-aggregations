af5b904b8fb368c81e0ae5b6e962350d61fa2792
Felipe Segovia <felipe.vsegovia@gmail.com>
2021-05-12 16:54:37 -0300

    MongoDB - Aggregations - Req03

--- scripts/generate_result.sh
+++ scripts/generate_result.sh
@@ -29,7 +29,7 @@ print_results() {
 
 # Print tests evaluation
 # for entry in "$TRYBE_DIR/expected-results"/*
-requisito=2
+requisito=3
 for i in $(seq $requisito $requisito)
 do
   entry="$TRYBE_DIR/expected-results/desafio$i"
