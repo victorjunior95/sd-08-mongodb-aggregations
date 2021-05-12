450281d3a06d233d03ab578f7fc360a156d3a0ff
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-05-12 18:23:43 -0300

    desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,43 @@
+db.movies.aggregate([
+  {
+    $match: {
+      countries: "USA",
+      "tomatoes.viewer.rating": { $gte: 3 },
+      cast: {
+        $in: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"],
+      },
+    },
+  },
+  {
+    $addFields: {
+      num_favs: {
+        $size: {
+          $setIntersection: [
+            "$cast",
+            ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"],
+          ],
+        },
+      },
+    },
+  },
+  {
+    $sort: {
+      num_favs: -1,
+      "tomatoes.viewer.rating": -1,
+      title: -1,
+    },
+  },
+  {
+    $project: {
+      title: 1,
+      num_favs: 1,
+      _id: 0,
+    },
+  },
+  {
+    $skip: 24,
+  },
+  {
+    $limit: 1,
+  },
+]);
