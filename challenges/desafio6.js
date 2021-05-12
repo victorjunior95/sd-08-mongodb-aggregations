32db1e56efca669f952cd3d5d91c40aab93f55f2
Cleberton <clebertonfgc@gmail.com>
2021-05-12 18:33:52 -0300

    Iniciando projeto

---
+++ challenges/desafio6.js
@@ -0,0 +1,22 @@
+db.movies.aggregate([{
+  $match: {
+    awards: {
+      $regex: /^won \d oscar./i,
+    },
+  } }, {
+  $group: {
+    _id: null,
+    maior_rating: { $max: "$imdb.rating" },
+    menor_rating: { $min: "$imdb.rating" },
+    media_rating: { $avg: "$imdb.rating" },
+    desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+  } }, {
+  $project: {
+    _id: 0,
+    maior_rating: { $max: "$maior_rating" },
+    menor_rating: { $min: "$menor_rating" },
+    media_rating: { $round: ["$media_rating", 1] },
+    desvio_padrao: { $round: ["$desvio_padrao", 1] },
+  },
+},
+]);
