ccbed6dbac678a39c60815aabaa733debf2a1cad
JRGCast <joao.ralph1@gmail.com>
2021-05-12 15:47:36 -0300

    feito desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,20 @@
+// Desafio 4
+// Nosso dataset de filmes tem muitos documentos diferentes,
+// alguns com títulos "mais complexos" do que outros.
+// Se quisermos analisar nossa coleção para encontrar títulos de filmes
+// que têm uma só palavra no título,
+// poderíamos buscar todos os filmes do dataset e processar isso na aplicação,
+// mas o Aggregation Framework nos permite fazer isso diretamente no lado do banco de dados.
+
+// Crie uma pipeline que retorna documentos
+// com o novo campo title_split, ela deve seguir as seguintes condições:
+// title_split deve conter uma lista de palavras presentes em title.
+// A pipeline deve retornar apenas filmes com o título composto apenas de uma palavra.
+// A pipeline deve ser ordenada por title em ordem alfabética.
+// Por exemplo, "Cinderela" e "3-25" devem entrar nessa contagem, mas "Cast Away" não.
+
+// Dica: utilize os operadores $split, $size e $sort para te auxiliar. Documentação do $split
+
+db.movies.aggregate([{ $project: { title_split: { $split: ["$title", " "] }, _id: 0 } }, { $match: { title_split: { $size: 1 } } }, { $sort: { title_split: 1 } }]);
+
+// Sua query deve retornar 8068 documentos.
