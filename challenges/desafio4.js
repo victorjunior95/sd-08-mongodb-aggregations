8b918ca7bf18854a6c3ff5dfe43dd237dc47c120
patriciavsilva <silva.patricia.v@gmail.com>
2021-05-12 20:18:49 -0300

    desafios 3 e 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-expressions
+db.movies.aggregate([
+  { $project: {
+    title_split: { $split: ["$title", " "] },
+  } },
+  {
+    $match: {
+      title_split: {
+        $size: 1,
+      },
+    } },
+  { $project: { _id: 0, title_split: 1 } },
+  { $sort: { title_split: 1 } },
+]);
