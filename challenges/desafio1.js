708dd6b67f00fa4d240148be65b45c6c939256d6
MEianelli <filipelouzano@gmail.com>
2021-05-12 16:45:24 -0300

    Requisito 1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -2,5 +2,4 @@ db.movies.aggregate([{ $match: { $and: [{ "imdb.rating": { $gte: 7 } },
   { $nor: [{ genres: "Crime" }, { genres: "Horror" }] },
   { $or: [{ rated: "PG" }, { rated: "G" }] },
   { $and: [{ languages: "English" }, { languages: "Spanish" }] },
-] } },
-{ $group: { _id: null, myCount: { $sum: 1 } } }]);
+] } }]);
