10628f08d3d43cc7d812593d6aa75c938519cd99
Leonardo <lecastro16@gmail.com>
2021-05-12 15:38:51 -0300

    requisito 4

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,3 +1,6 @@
 db.movies.aggregate([
-  { $project: { _id: 1, title_split: { $split: ["$title", " "] } } },
+  { $addFields: { title_split: { $split: ["$title", " "] } } },
+  { $match: { title_split: { $size: 1 } } },
+  { $project: { _id: 0, title_split: 1 } },
+  { $sort: { title_split: 1 } },
 ]);
