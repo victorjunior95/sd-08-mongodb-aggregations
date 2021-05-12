b7b758f6b0ac717a6dad283c96d4975cec0f178b
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-05-12 16:36:51 -0300

    desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,39 @@
+db.trips.aggregate(
+  [
+    {
+      $addFields: {
+        duracao: {
+          $subtract: ["$stopTime", "$startTime"],
+        },
+      },
+    },
+    {
+      $addFields: {
+        duracao2: {
+          $divide: ["$duracao", 3600000],
+        },
+      },
+    },
+    {
+      $group: {
+        _id: "$usertype",
+        duracaoMedia: {
+          $avg: "$duracao2",
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        duracaoMedia: {
+          $round: ["$duracaoMedia", 2],
+        },
+      },
+    },
+    {
+      $sort: {
+        duracaoMedia: 1,
+      },
+    },
+  ],
+);
