ad983aa48067b3619468b35cbb65d58eae4632c4
Gabriel Donnantuoni Lima <gabrieldonnantuoni@gmail.com>
2021-05-12 19:18:31 -0300

    Desafio 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,7 @@
+db.trips.aggregate([
+  { $set: { diaDaSemana: { $dayOfWeek: "$startTime" } } },
+  { $group: { _id: "$diaDaSemana", total: { $sum: 1 } } },
+  { $project: { _id: 0, diaDaSemana: "$_id", total: 1 } },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
