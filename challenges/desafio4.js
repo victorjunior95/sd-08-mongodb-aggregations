9e448fcf54886ac5006d2638fc1e6cab8d52b108
Ediberto Oliveira <edibertooliveira@aol.com>
2021-05-12 18:53:03 -0300

    feat: requeriment 04

---
+++ challenges/desafio4.js
@@ -0,0 +1,26 @@
+db.movies.aggregate([{
+    $addFields: {
+      title_split: {
+        $split: ["$title", " "],
+      },
+    },
+  },
+  {
+    $match: {
+      $expr: {
+        $eq: [{ $size: "$title_split" }, 1],
+      },
+    },
+  },
+  {
+    $sort: {
+      title: 1,
+    },
+  },
+  {
+    $project: {
+      title_split: 1,
+      _id: 0,
+    },
+  }
+]);
