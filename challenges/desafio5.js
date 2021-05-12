32f20659e8c731ba1d6c1bbc4f1256cb869c49f7
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-05-12 14:27:28 -0300

    linter fixes

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -7,41 +7,41 @@ const favoriteActors = [
 ];
 
 db.movies.aggregate(
-[
-  {
-    $match: {
-      countries:"USA",
-      "tomatoes.viewer.rating": { $gte: 3 },
-      cast: { $exists: true },
-    }
-  },
-  {
-    $addFields: {
-      num_favs: {
-        $size: {
-          $setIntersection: ["$cast", favoriteActors],
-        }
-      }
-    }
-  },
-  {
-    $sort: {
-      num_favs: -1,
-      "tomatoes.viewer.rating": -1,
-      title: -1
-    }
-  },
-  {
-    $skip: 24
-  },
-  {
-    $limit: 1
-  },
-  {
-    $project: {
-      _id: 0,
-      title: 1
-    }
-  }
-]
+  [
+    {
+      $match: {
+        countries: "USA",
+        "tomatoes.viewer.rating": { $gte: 3 },
+        cast: { $exists: true },
+      },
+    },
+    {
+      $addFields: {
+        num_favs: {
+          $size: {
+            $setIntersection: ["$cast", favoriteActors],
+          },
+        },
+      },
+    },
+    {
+      $sort: {
+        num_favs: -1,
+        "tomatoes.viewer.rating": -1,
+        title: -1,
+      },
+    },
+    {
+      $skip: 24,
+    },
+    {
+      $limit: 1,
+    },
+    {
+      $project: {
+        _id: 0,
+        title: 1,
+      },
+    },
+  ],
 );
