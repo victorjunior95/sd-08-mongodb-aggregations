08b31bd1c1ee99de1c683a2dadbb30cf9d4e6885
Seu nome <luiza.dilly@gmail.com>
2021-05-12 15:09:20 -0300

    fix req 4

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,5 +1,6 @@
 db.movies.aggregate(
-  { $project: { title_split: { $split: ["$title", " "] } } },
+  { $addFields: { title_split: { $split: ["$title", " "] } } },
   { $match: { title_split: { $size: 1 } } },
+  { $project: { _id: 0, title_split: 1 } },
   { $sort: { title_split: 1 } },
 );
