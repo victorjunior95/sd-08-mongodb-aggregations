467618794d71fe206b05cb6e45b9808945afe3e8
jnpefo <jnpefo@gmail.com>
2021-05-12 16:19:55 -0300

    Req 3

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,9 +1,9 @@
 db.movies.aggregate([
   {
     $match: {
-      rating: { $gte: 7 },
-      genero: { $nin: ["Crime", "Horror"] },
-      clasificacao: { $in: ["PG", "G"] },
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
       languages: { $all: ["English", "Spanish"] },
     },
   },
---
+++ challenges/desafio3.js
@@ -0,0 +1,26 @@
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      titulo: "$title",
+      avaliado: "$year",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$imdb.year",
+    },
+  },
+  {
+    $sort: {
+      notaIMDB: -1,
+      title: 1,
+    },
+  },
+]);
