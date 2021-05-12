d7fa49f9189ee6f807884ab121299b9a5b3a4679
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-05-12 17:02:58 -0300

    Challenge 2 done

---
+++ challenges/desafio2.js
@@ -0,0 +1,24 @@
+// Solution to rename keys inside $project found at:
+// https://stackoverflow.com/questions/23784370/how-do-i-rename-fields-when-performing-search-projection-in-mongodb
+
+db.movies.aggregate(
+  [{
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+  ],
+);
