6f1f9f9c8e4504ea20d368b8db90d29aaf89ea93
Lucas Neiva <lucassneiva.code@gmail.com>
2021-05-12 20:00:08 -0300

    fix lint 2 and 3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,10 +1,9 @@
 db.movies.aggregate([
-    { $match: { "imdb.rating": { $gte: 7 },
-       genres: { $nin: ["Crime", "Horror"] },
-       rated: { $in: ["PG", "G"] },
-       languages: { $all: ["English", "Spanish"] } }
-     },
-     { $project: { _id: 0, titulo: "$title", avaliado: "$rated", notaIMDB: "$imdb.rating", votosIMDB: "$imdb.votes", ano: "$year" } },
-     { $sort: {  titulo: 1, year: -1, notaIMDB: -1 } }
-   ]);
-   
+  { $match: { "imdb.rating": { $gte: 7 },
+    genres: { $nin: ["Crime", "Horror"] },
+    rated: { $in: ["PG", "G"] },
+    languages: { $all: ["English", "Spanish"] } }
+  },
+  { $project: { _id: 0, titulo: "$title", avaliado: "$rated", notaIMDB: "$imdb.rating", votosIMDB: "$imdb.votes", ano: "$year" } },
+  { $sort: {  titulo: 1, year: -1, notaIMDB: -1 } }
+]);
