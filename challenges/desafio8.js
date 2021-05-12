6684bd23dcc198c6e331c24a06db1a1deea73cf2
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-05-12 15:58:44 -0300

    desafio 8 corigido

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,5 +1,4 @@
 // solucao para encontarr arrays nao vazios retirada daqui: https://stackoverflow.com/questions/7811163/query-for-documents-where-array-size-is-greater-than-1
-
 db.air_alliances.aggregate(
   [
     {
@@ -13,21 +12,22 @@ db.air_alliances.aggregate(
         },
         pipeline: [
           {
-            $match: { $expr: {
-              $eq: ["$$airline", "$airline.name"],
-            },
-            },
-          },
-          {
             $match: {
-              airplane: {
-                $in: ["747", "380"],
-              },
+              $expr: { $eq: ["$$airline", "$airline.name"] },
+              airplane: { $in: ["747", "380"] },
             },
           },
         ],
         as: "routeInfo",
       },
+    },
+    {
+      $match: {
+        "routeInfo.1": { $exists: true }
+      }
+    },
+    {
+      $unwind: "$routeInfo"
     },
     {
       $group: {
