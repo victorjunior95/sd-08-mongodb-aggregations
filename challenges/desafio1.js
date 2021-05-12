f2d25bfdc2665c3810ce1d980ec7c9943ba5eaf5
Leandro Lachini <leandrolachini@gmail.com>
2021-05-12 15:19:06 -0300

    desafio 1

---
+++ challenges/desafio1.js
@@ -0,0 +1,13 @@
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    } },
+  { $group: {
+    _id: null,
+    totalDeFilmesPesquisados: { $sum: 1 },
+  },
+  }]);
