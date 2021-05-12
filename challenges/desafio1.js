aabb07ccfd6622754c6b56e76dc8051af83cfdbf
JRGCast <joao.ralph1@gmail.com>
2021-05-12 15:03:26 -0300

    falou retornar 41 docs, diferente das questoes seguintes, logo, colocado 'count' ao desafio 1 com o alias 'total'

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -10,6 +10,6 @@
 // languages contém English e Spanish.
 // Utilize a coleção movies.
 
-db.movies.aggregate([{ $match: { $and: [{ "imdb.rating": { $gte: 7 } }, { genres: { $nin: ["Crime", "Horror"] } }, { rated: { $in: ["PG", "G"] } }, { languages: { $all: ["English", "Spanish"] } }] } }]);
+db.movies.aggregate([{ $match: { $and: [{ "imdb.rating": { $gte: 7 } }, { genres: { $nin: ["Crime", "Horror"] } }, { rated: { $in: ["PG", "G"] } }, { languages: { $all: ["English", "Spanish"] } }] } }, { $count: "total" }]);
 
 // Sua query deve retornar 41 documentos.
