7c47fc0d3f3371eaa295e3a849b0575cccca9b27
MEianelli <filipelouzano@gmail.com>
2021-05-12 18:31:18 -0300

    Requisito 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,1 +1,15 @@
-db;
+db.movies.aggregate([
+  { $match: { countries: "USA" } },
+  { $match: { "tomatoes.viewer.rating": { $gte: 3 } } },
+  { $match: { cast: { $ne: null } } },
+  { $addFields: { favActors: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"] } },
+  { $project: { _id: 0,
+    num_favs: { $size: { $setIntersection: ["$favActors", "$cast"] } },
+    "tomatoes.viewer.rating": 1,
+    title: 1,
+  } },
+  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
+  { $skip: 24 },
+  { $limit: 1 },
+  { $project: { _id: 0, title: 1 } },
+]);
