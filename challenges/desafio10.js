17fafc3e0fdc97f4b9ed1be912f20c4c5c71bc70
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-05-12 18:29:44 -0300

    Conclui desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,25 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      duracaoViagem: {
+        $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3600000],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: { $avg: "$duracaoViagem" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: ["$duracaoMedia", 2] },
+    },
+  },
+  {
+    $sort: { duracaoMedia: 1 },
+  },
+]);
