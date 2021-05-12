e8393b7fd9c89b1ed2e1b7d89645aa8f1687c515
emassaki <massakihirayama@gmail.com>
2021-05-12 16:54:47 -0300

    all reqs

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,14 +2,20 @@ db.trips.aggregate([
   {
     $group: {
       _id: "$usertype",
-      duracaoMedia: { $avg: { $subtract: ["$stopTime", "$startTime"] } },
+      duracaoMedia: {
+        $avg: {
+          $subtract: ["$stopTime", "$startTime"],
+        },
+      },
     },
   },
   {
     $project: {
       _id: 0,
       tipo: "$_id",
-      duracaoMedia: { $round: [{ $divide: ["$duracaoMedia", 3600000] }, 2] },
+      duracaoMedia: {
+        $round: [{ $divide: ["$duracaoMedia", 3600000] }, 2],
+      },
     },
   },
 ]);
