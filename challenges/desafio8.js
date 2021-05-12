131789d3e65369ff126b53290758fe20b1a893a8
Seu nome <luiza.dilly@gmail.com>
2021-05-12 17:33:00 -0300

    req8

---
+++ challenges/desafio8.js
@@ -0,0 +1,16 @@
+db.air_routes.aggregate(
+  { $lookup: {
+    from: "air_alliances",
+    localField: "airline.name",
+    foreignField: "airlines",
+    as: "aircompany",
+  } },
+  { $match: {
+    airplane: { $in: ["747", "380"] },
+    "aircompany.name": { $exists: true },
+  } },
+  { $unwind: "$aircompany" },
+  { $group: { _id: "$aircompany.name", totalRotas: { $sum: 1 } } },
+  { $sort: { totalRotas: -1 } },
+  { $limit: 1 },
+);
