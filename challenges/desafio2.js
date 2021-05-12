e9882d05cebe08daf9905350f688e5274f29923b
Igor <igorln96@gmail.com>
2021-05-12 15:20:10 -0300

    fix req 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -2,12 +2,10 @@ db.movies.aggregate(
   [
     {
       $match: {
-        $and: [
-          { "imdb.rating": { $gte: 7 } },
-          { genre: { $nin: ["Crime", "Horror"] } },
-          { rated: { $in: ["PG", "G"] } },
-          { languages: { $all: ["English", "Spanish"] } },
-        ],
+        "imdb.rating": { $gte: 7 },
+        genre: { $nin: ["Crime", "Horror"] },
+        rated: { $in: ["PG", "G"] },
+        languages: { $all: ["English", "Spanish"] },
       },
     },
     {
