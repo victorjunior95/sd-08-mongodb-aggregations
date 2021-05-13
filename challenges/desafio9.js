4287eb57c700ff6a39cde19b5a29a8fa5671e17f
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-05-12 23:47:23 -0300

    req10

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -8,8 +8,12 @@ db.trips.aggregate(
     { $group:
       {
         _id: null,
-        maiorAnoNascimento: { $max: "$birthYear" },
-        menorAnoNascimento: { $min: "$birthYear" },
+        maiorAnoNascimento: { $max:
+          { $toInt: "$birthYear" },
+        },
+        menorAnoNascimento: { $min:
+          { $toInt: "$birthYear" },
+        },
       },
     },
     { $project:
