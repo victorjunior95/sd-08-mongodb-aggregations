6f4c7122136fdcd9ca3e0f89b9e7363bfe71d1b9
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-05-13 09:17:47 -0300

    Enviando desafio 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,27 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      diaDaSemana: { $dayOfWeek: "$startTime" },
+    },
+  },
+  {
+    $group: {
+      _id: {
+        diaDaSemana: "$diaDaSemana",
+        nomeEstacao: "$startStationName",
+      },
+      total: {
+        $sum: 1,
+      },
+    },
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id.nomeEstacao",
+      total: "$total",
+    },
+  },
+]);
