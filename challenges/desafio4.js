d6a5bdce1cc3d5e26da2ee631349ac5aea0736c0
amandaos <amanda.775.souza@gmail.com>
2021-05-13 19:16:38 -0300

    test

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,4 +1,4 @@
 db.movies.aggregate([{ $addFields: { title_split: { $split: ["$title", " "] } } },
-  { $project: { _id: 0, title_split: 1 } },
-  { $match: { $title_split: { $size: 1 } } },
-  { $sort: { $title_split: 1 } }]);
+  { $match: { title_split: { $size: 1 } } },
+  { $sort: { $title_split: 1 } },
+  { $project: { _id: 0, title_split: 1 } }]);
