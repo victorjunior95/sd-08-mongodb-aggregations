28a1aaf5e4f00ee8af2ddbc0a1fe76fee299ce3d
Paulo Simões <paulohbsimoes@gmail.com>
2021-05-12 21:14:54 -0300

    Desafio 10 concluído

---
+++ challenges/desafio10.js
@@ -0,0 +1,26 @@
+const HOUR = 60 * 60 * 1000;
+db.trips.aggregate(
+  {
+    $addFields: {
+      duracao: {
+        $divide: [
+          { $subtract: ["$stopTime", "$startTime"] },
+          HOUR,
+        ],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: { $avg: "$duracao" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: ["$duracaoMedia", 2] },
+    },
+  },
+);
