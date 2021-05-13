3de15562ad1c328408a66307e6ffec06cd3dafcf
Cleberton <clebertonfgc@gmail.com>
2021-05-12 23:24:41 -0300

    Desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,31 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      diff: { $subtract: ["$stopTime", "$startTime"] },
+    },
+  },
+  {
+    $addFields: {
+      result: {
+        $divide: ["$diff", 3600000],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$usertype",
+      media: {
+        $avg: "$result",
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      media: {
+        $round: ["$media", 2],
+      },
+    },
+  }, { $sort: { media: 1 } },
+]);
