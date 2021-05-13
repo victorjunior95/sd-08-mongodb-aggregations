a36ccc1787112b0e8b1075bcb130d51e21ceda94
victor718 <victorfelipeoliveira1@gmail.com>
2021-05-13 11:09:05 -0300

    desafio 06

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -11,7 +11,8 @@ db.movies.aggregate([
       _id: null,
       maior_rating: { $max: "$imdb.rating" },
       menor_rating: { $min: "$imdb.rating" },
-      media_rating: { $round: [{ $avg: "$imdb.rating" }, 2] },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
     },
   },
   {
@@ -19,7 +20,8 @@ db.movies.aggregate([
       _id: 0,
       maior_rating: 1,
       menor_rating: 1,
-      media_rating: 1,
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
     },
   },
 ]);
