ec2a58a050cb73fd3378082010acec30fcaafaca
Bruno Martins <bruno.martins217@gmail.com>
2021-05-13 00:41:27 -0300

    Desafio 7

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,5 +1,5 @@
 db.movies.aggregate([
-  { $match: { countries: { $eq: "USA" } } },
+  { $match: { languages: "English" } },
   { $unwind: "$cast" },
   { $group: {
     _id: "$cast",
@@ -7,5 +7,5 @@ db.movies.aggregate([
     mediaIMDB: { $avg: "$imdb.rating" },
   } },
   { $project: { _id: 1, numeroFilmes: 1, mediaIMDB: { $round: ["$mediaIMDB", 1] } } },
-  { $sort: { cast: -1, numeroFilmes: -1 } },
+  { $sort: { numeroFilmes: -1, _id: -1 } },
 ]);
