7e1db9a56e6da0d8b78969bc705cdbdd8a8e5e94
jnpefo <jnpefo@gmail.com>
2021-05-13 17:23:06 -0300

    Req 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,39 @@
+db.air_alliances.aggregate([
+  {
+    $unwind: "$airlines",
+  },
+  {
+    $lookup: {
+      from: "air_routes",
+      localField: "airlines",
+      foreignField: "airline.name",
+      as: "rotas",
+    },
+  },
+  {
+    $unwind: "$rotas",
+  },
+  {
+    $match: {
+      "rotas.airplane": {
+        $in: ["747", "380"],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$name",
+      totalRotas: {
+        $sum: 1,
+      },
+    },
+  },
+  {
+    $sort: {
+      totalRotas1: -1,
+    },
+  },
+  {
+    $limit: 1,
+  },
+]);
