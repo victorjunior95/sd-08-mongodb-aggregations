1a67379dcb874b7d40405476ad34e8f4aa00047b
Mateus Coury <mateuscoury@gmail.com>
2021-05-13 15:46:30 -0300

    9 12 e 13

---
+++ challenges/desafio12.js
@@ -0,0 +1,5 @@
+db.trips.aggregate([
+  { $addFields: { diaDasemana: { $dayOfWeek: "$startTime" } } },
+  { $match: { diaDasemana: 5 } },
+  { $group: { _id: "$startStationName", total: { $sum: 1 } } },
+  { $project: { _id: 0, nomeEstacao: "$_id", total: "$total" } }, { $sort: { total: -1 } }, { $limit: 1 }]);
