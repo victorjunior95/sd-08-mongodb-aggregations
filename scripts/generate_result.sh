488cdd5fcc309f435fd9b839520af369b6cc2e43
Digo-Castro <digo_castro@outlook.com>
2021-05-13 01:19:44 -0300

    [Desafio 12]  - OK

--- scripts/generate_result.sh
+++ scripts/generate_result.sh
@@ -30,7 +30,7 @@ print_results() {
 # Print tests evaluation
 # Inicio da modificação <==
 # for entry in "$TRYBE_DIR/expected-results"/*  ===  DESCOMENTAR
-requisito=11
+requisito=12
 for i in $(seq $requisito $requisito)
 do
   entry="$TRYBE_DIR/expected-results/desafio$i"
