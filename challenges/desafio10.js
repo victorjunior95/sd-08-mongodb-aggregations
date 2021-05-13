72a631f14ebb415e2b4c59f72be825cbe43cf601
Thiago Ferreira <thiagocristhian@gmail.com>
2021-05-13 16:50:19 -0300

    Requisito 10 - Corrigindo requisito

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -3,6 +3,7 @@ db.trips.aggregate([
     _id: "$usertype",
     duracaoMedia: { $avg: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3600000] } },
   } },
+  { $sort: { duracaoMedia: 1 } },
   { $project: {
     _id: 0,
     tipo: "$_id",
