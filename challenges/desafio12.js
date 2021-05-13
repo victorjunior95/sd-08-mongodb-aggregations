a98e83de2619f49e9e235cd9d28e368087428fb5
Paulo Simões <paulohbsimoes@gmail.com>
2021-05-12 21:14:54 -0300

    Desafio 12 concluído

---
+++ challenges/desafio12.js
@@ -0,0 +1,63 @@
+// MongoError: Total size of documents in trips matching pipeline's
+// $lookup stage exceeds 104857600 bytes
+// Vlwww Rosiii, por me ajudar a descobrir o que era esse problema :D
+
+db.trips.aggregate(
+  {
+    $addFields: {
+      diaDaSemana: {
+        $dayOfWeek: "$startTime",
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$diaDaSemana",
+      count: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$count",
+    },
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+  {
+    $lookup: {
+      from: "trips",
+      let: { dia: "$diaDaSemana" },
+      pipeline: [
+        {
+          $match: {
+            $expr: {
+              $eq: [
+                "$$dia",
+                { $dayOfWeek: "$startTime" },
+              ],
+            },
+          },
+        },
+        {
+          $group: {
+            _id: "$startStationName",
+            total: { $sum: 1 },
+          },
+        },
+      ],
+      as: "estacoes",
+    },
+  },
+  { $unwind: "$estacoes" },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$estacoes._id",
+      total: "$estacoes.total",
+    },
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+);
