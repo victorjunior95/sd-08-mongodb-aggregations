098fd577e4e3dbb991329cb0c96e9970281fb495
Cleberton <clebertonfgc@gmail.com>
2021-05-13 15:22:05 -0300

    Desafio 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,33 @@
+use("aggregations");
+db.air_alliances.aggregate([
+  {
+    $unwind: "$airlines",
+  },
+  {
+    $lookup: {
+      from: "air_routes",
+      localField: "airlines",
+      foreignField: "airline.name",
+      as: "Aliance",
+    },
+  },
+  {
+    $unwind: "$Aliance",
+  },
+  {
+    $match: {
+      "Aliance.airplane": { $in: ["747", "380"],
+      },
+    },
+  },
+  {
+    $group: { _id: "$name", totalRotas: { $sum: 1 } },
+  },
+  {
+    $sort: { totalRotas: -1 },
+  },
+  {
+
+    $limit: 1,
+  },
+]);
