a224b1e82763547bd444821ad4af3e8a857765be
amandaos <amanda.775.souza@gmail.com>
2021-05-13 20:03:22 -0300

    nao aguemto mais refazer o desafio 9, pelo amor de deus

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,4 +1,4 @@
 db.trips.aggregate([{ $match: { birthYear: { $ne: "" } } },
-  { $group: { _id: null, maiorAnoNascimento: { $max: { $toInt: "$birthYear" }, menorAnoNascimento: { $min: { toInt: "$birthYear" } } } } },
+  { $group: { _id: null, maiorAnoNascimento: { $max: { $toInt: "$birthYear" } }, menorAnoNascimento: { $min: { $toInt: "$birthYear" } } } },
   { $project: { _id: 0, maiorAnoNascimento: 1, menorAnoNascimento: 1 } },
 ]);
