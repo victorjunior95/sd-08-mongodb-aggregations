9c18e2263c3a444644ad143577751db84ca98220
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-05-13 17:46:21 -0300

    desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,33 @@
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: {
+        $gte: ISODate("2016-03-10"),
+        $lt: ISODate("2016-03-11"),
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "",
+      duracaoMediaEmMinutos: {
+        $avg: {
+          $divide: [
+            { $subtract: ["$stopTime", "$startTime"] },
+            60000,
+          ],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      duracaoMediaEmMinutos: {
+        $round: [
+          "$duracaoMediaEmMinutos",
+        ],
+      },
+    },
+  },
+]);
