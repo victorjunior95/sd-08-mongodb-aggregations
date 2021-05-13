17408ba43261fa55669aaafb85c6d896ebf724ca
Rita Jeveaux <ritajeveaux@gmail.com>
2021-05-12 21:12:37 -0300

    Requisito 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,18 +1,9 @@
 const atores = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
 db.movies.aggregate([
-  { $match:
-    { countries: "USA",
-      "tomatoes.viewer.rating": { $gte: 3 } },
-  },
-  { $addFields: {
-    num_favs: { $size: { $setIntersection: [atores, "$cast"] } } },
-  },
-  { $sort:
-    { num_favs: -1, "_id.viewer_rating": -1, "_id.title": -1 },
-  },
-  { $project:
-    { title: "$_id.title", _id: 0 },
-  },
+  { $match: { countries: { $in: ["USA"] }, "tomatoes.viewer.rating": { $gte: 3 }, cast: { $exists: true } } },
+  { $addFields: { num_favs: { $size: { $setIntersection: [atores, "$cast"] } } } },
+  { $sort: { num_favs: -1, "_id.viewer_rating": -1, "_id.title": -1 } },
+  { $project: { title: 1, _id: 0 } },
   { $skip: 24 },
   { $limit: 1 },
 ]);
