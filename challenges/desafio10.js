7d6de9af402cbbb0ecafcb7fa53a52a41e32db1c
JRGCast <joao.ralph1@gmail.com>
2021-05-13 17:04:31 -0300

    feito desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,12 @@
+// Desafio 10
+// Encontre a média de viagens por tipo de usuário.
+// Exiba o valor em horas com apenas duas casas decimais
+// Exiba a média de viagens ordenada de forma crescente.
+// Para arredondar a média use o $round.
+
+// O resultado da sua query deve ter o seguinte formato:
+
+// { "tipo" : <tipo>, "duracaoMedia" : <duracaoMedia> }
+const ONE_HOUR_IN_MILISECONDS = 3600000;
+
+db.trips.aggregate([{ $group: { _id: "$usertype", duracaoMedia: { $avg: { $subtract: ["$stopTime", "$startTime"] } } } }, { $project: { tipo: "$id", duracaoMedia: { $round: [{ $divide: ["$duracaoMedia", ONE_HOUR_IN_MILISECONDS] }, 2] } } }, { $sort: { duracaoMedia: 1 } }]);
