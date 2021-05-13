52fb9970a69951d3b7bad2cdf2a376f04876e721
Luis Felipe Avila <lfelipeavila@outlook.com>
2021-05-13 02:35:52 -0300

    Req 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,14 @@
+db.movies.aggregate([{ $match: {
+  "imdb.rating": { $gte: 7 },
+  $nor: [{ genres: "Crime" }, { genres: "Horror" }],
+  $or: [{ rated: "PG" }, { rated: "G" }],
+  $and: [{ languages: "English" }, { languages: "Spanish" }] },
+}, { $sort: { year: -1, "imdb.rating": -1, title: 1 } },
+{ $project: {
+  _id: 0,
+  titulo: "$title",
+  avaliado: "$rated",
+  notaIMDB: "$imdb.rating",
+  votosIMDB: "$imdb.votes",
+  ano: "$year" } },
+]);
