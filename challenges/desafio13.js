c624f50c2450bca64e990e31533f1f0c8982e846
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-05-13 18:24:07 -0300

    Requisito 13 - correcao da identacao

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,23 +1,23 @@
 const convertMinutes = 60000;
 db.trips.aggregate([
-{
-  $match: {
-    startTime: {
-      $gt: ISODate("2016-03-10"),
-      $lt: ISODate("2016-03-11"),
-    },
-  },
-},
   {
-  $group: {
-    _id: null,
-    duracaoMedia: {
-      $avg: {
-        $subtract: ["$stopTime", "$startTime"],
+    $match: {
+      startTime: {
+        $gt: ISODate("2016-03-10"),
+        $lt: ISODate("2016-03-11"),
       },
     },
   },
-},
+  {
+    $group: {
+      _id: null,
+      duracaoMedia: {
+        $avg: {
+          $subtract: ["$stopTime", "$startTime"],
+        },
+      },
+    },
+  },
   {
     $project: {
       _id: 0,
