9aaae61df9b04dfdeca1c11f0b8d040e553455bb
amandaos <amanda.775.souza@gmail.com>
2021-05-13 18:44:13 -0300

    feature: desafios 7 e 11

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,3 +1,3 @@
-db.movies.aggregate([{ $project: { _id: 0, title_split: { $split: ["title", ""] } } },
+db.movies.aggregate([{ $project: { _id: 0, title_split: { $split: ["$title", ""] } } },
   { $match: { $title_split: { $size: 1 } } },
   { $sort: { $title_split: 1 } }]);
---
+++ challenges/desafio7.js
@@ -0,0 +1,4 @@
+db.movies.aggregate([{ $match: { language: "English" } }, { $unwind: "$cast" },
+  { $group: { _id: "$cast", numeroFilmes: { $sum: 1 }, media: { $avg: "$imdb.rating" } } },
+  { $project: { _id: 1, numeroFilmes: 1, mediaIMDB: { $round: ["$mediaIMDB", 1] } } },
+  { $sort: { _id: -1, numeroFilmes: -1 } }]);
