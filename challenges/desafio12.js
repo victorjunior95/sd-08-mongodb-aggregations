e433d0d7735aba95568fa16c9073e117f2fce9fb
Alvaro Braga <alvaro.braga.job@gmail.com>
2021-05-12 23:39:00 -0300

    Req 11-12

---
+++ challenges/desafio12.js
@@ -0,0 +1,8 @@
+db.trips.aggregate([
+  { $addFields: { diaDaSemana: { $dayOfWeek: "$startTime" } } },
+  { $match: { diaDaSemana: 5 } },
+  { $group: { _id: "$startStationName", count: { $sum: 1 } } },
+  { $project: { _id: 0, nomeEstacao: "$_id", total: "$count" } },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
