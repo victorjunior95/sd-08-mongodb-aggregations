cbc2cd25150e90dec65363e28ec0c5ae939d2ba9
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-05-13 10:59:42 -0300

    Acerta desafios 11 e 12

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,24 +1,24 @@
 db.trips.aggregate([
   {
     $addFields: {
-      dayOfWeek: { $dayOfWeek: "$startTime" },
+      diaDaSemana: { $dayOfWeek: "$startTime" },
     },
   },
   {
     $group: {
-      _id: "$dayOfWeek",
+      _id: "$diaDaSemana",
       total: { $sum: 1 },
     },
   },
   {
-    $sort: { total: -1 },
+    $project: {
+      diaDaSemana: "$_id",
+      total: "$total",
+      _id: 0,
+    },
   },
   {
-    $project: {
-      _id: 0,
-      diaDaSemana: "$_id",
-      total: 1,
-    },
+    $sort: { total: -1 },
   },
   {
     $limit: 1,
--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -20,7 +20,7 @@ db.trips.aggregate([
     $project: {
       _id: 0,
       nomeEstacao: "$_id.stationName",
-      total: 1,
+      total: "$total",
     },
   },
   {
