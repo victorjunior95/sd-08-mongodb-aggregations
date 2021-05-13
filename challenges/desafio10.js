9c690c724e055cd2df335bb0d884de3c75b6f016
Aline Debastiani <debastianialine@gmail.com>
2021-05-13 15:58:45 -0300

    cumpre desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,15 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$usertype",
+      dateDifference: { $avg: { $subtract: ["$stopTime", "$startTime"] } },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: [{ $divide: ["$dateDifference", 60 * 60 * 1000] }, 2] },
+    },
+  },
+]);
