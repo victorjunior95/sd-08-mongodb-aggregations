c14c437424e2cfeb500e85964af7afa48c77308f
devFelipeFranca <felipefrancafj@gmail.com>
2021-05-13 15:49:31 -0300

    Updating...

---
+++ challenges/desafio6.js
@@ -0,0 +1,44 @@
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        {
+          awards: /Won/,
+        },
+        {
+          awards: /Oscar/,
+        },
+      ],
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      max: {
+        $max: "$imdb.rating",
+      },
+      min: {
+        $min: "$imdb.rating",
+      },
+      avg: {
+        $avg: "$imgb.rating",
+      },
+      stdDevSamp: {
+        $stdDevSamp: "$imdb.rating",
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maior_rating: "$max",
+      menor_rating: "$min",
+      media_rating: {
+        $round: ["$avg", 1],
+      },
+      desvio_padrao: {
+        $round: ["$steDevSamp", 1],
+      },
+    },
+  },
+]);
