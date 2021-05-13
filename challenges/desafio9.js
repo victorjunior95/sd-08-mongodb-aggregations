4cb45d497855f893580e1729f9fffa2d2e3910e7
Digo-Castro <digo_castro@outlook.com>
2021-05-12 22:13:09 -0300

    [Desafio 9]  - OK

---
+++ challenges/desafio9.js
@@ -0,0 +1,9 @@
+db.trips.aggregate([
+  { $match: { birthYear: { $gt: 0 } } },
+  { $group: {
+    _id: null,
+    maiorAnoNascimento: { $max: "$birthYear" },
+    menorAnoNascimento: { $min: "$birthYear" },
+  } },
+  { $project: { _id: 0 } },
+]);
--- scripts/generate_result.sh
+++ scripts/generate_result.sh
@@ -30,7 +30,7 @@ print_results() {
 # Print tests evaluation
 # Inicio da modificação <==
 # for entry in "$TRYBE_DIR/expected-results"/*  ===  DESCOMENTAR
-requisito=8
+requisito=9
 for i in $(seq $requisito $requisito)
 do
   entry="$TRYBE_DIR/expected-results/desafio$i"
