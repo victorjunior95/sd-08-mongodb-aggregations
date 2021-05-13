c7191c3406eaf62122acb9f86865e10152b1e291
Uilton Rocha <itoclick@gmail.com>
2021-05-13 02:05:36 -0300

    desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,24 @@
+db.movies.aggregate([
+  {
+    $match: { $and: [
+      { "imdb.rating": { $gte: 7 } },
+      { genres: { $nin: ["Crime", "Horror"] } },
+      { languages: { $all: ["English", "Spanish"] } },
+      { rated: { $in: ["PG", "G"] } },
+    ] },
+  },
+  {
+    $project: {
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+      _id: 0,
+    },
+
+  },
+  {
+    $sort: { year: -1, "$imdb.rating": -1 },
+  },
+]);
