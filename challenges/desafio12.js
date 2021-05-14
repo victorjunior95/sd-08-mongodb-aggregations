63f09196d0e8af4ac82f903a5a0a2f27c9e2f328
jnpefo <jnpefo@gmail.com>
2021-05-14 13:37:22 -0300

    Req 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,23 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: { dia: { $dayOfWeek: "$startTime" }, nome: "$startStationName" },
+      total: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id.nome",
+      total: "$total",
+    },
+  },
+  {
+    $sort: {
+      total: -1,
+    },
+  },
+  {
+    $limit: 1,
+  },
+]);
