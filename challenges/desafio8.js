8f1d8ff0ef184e990fe63742f51810a12995830a
Luis Felipe Avila <lfelipeavila@outlook.com>
2021-05-14 14:35:31 -0300

    Req 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,13 @@
+db.air_alliances.aggregate([{ $unwind: "$airlines" },
+  { $lookup: {
+    from: "air_routes",
+    localField: "airlines",
+    foreignField: "airline.name",
+    as: "data",
+  } }, { $unwind: "$data" },
+  { $match: {
+    "data.airplane": { $in: ["747", "380"] },
+  } },
+  { $group: { _id: "$name", totalRotas: { $sum: 1 } } },
+  { $sort: { totalRotas: -1 } }, { $limit: 1 },
+]);
