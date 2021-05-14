6fbe825795e6401ad6ed5d77901496452c5f6da7
Uilton Rocha <itoclick@gmail.com>
2021-05-14 10:09:32 -0300

    desafio 6

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -8,16 +8,23 @@ const lista = [
 db.movies.aggregate([
   {
     $match: {
-      countries: "USA" ,
-      "tomatoes.viewer.rating": { $gte: 3 } ,
-      cast:{$exists:true},
-      
+      countries: "USA",
+      "tomatoes.viewer.rating": { $gte: 3 },
+      cast: { $exists: true },
+
     },
   },
-  { $project: { num_favs: {$size: {
-    $setIntersection: ["$cast", lista] }}, title: 1, "tomatoes.viewer.rating": 1, _id:0 } },
+  {
+    $project: {
+      num_favs: {
+        $size: {
+          $setIntersection: ["$cast", lista]
+        }
+      }, title: 1, "tomatoes.viewer.rating": 1, _id: 0
+    }
+  },
   { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
-  {$skip:24},
-  {$limit:1},
-  {$project:{ title: 1}}
+  { $skip: 24 },
+  { $limit: 1 },
+  { $project: { title: 1 } }
 ]);
