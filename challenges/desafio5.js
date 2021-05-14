74cf611acf3c2f1d58266c875daccbe976e27dca
Lucas Neiva <lucassneiva.code@gmail.com>
2021-05-13 21:48:07 -0300

    fix lint - 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,16 +1,16 @@
-const fav_actors = [
-"Sandra Bullock",
-"Tom Hanks",
-"Julia Roberts",
-"Kevin Spacey",
-"George Clooney"];
+const favActors = [
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney"];
 
 db.movies.aggregate([
-  { $match: 
-    { cast: { $exists: true }, 
-    countries: { $eq: "USA" },
-    "tomatoes.viewer.rating": { $gte: 3 } } },
-  { $addFields: { num_favs: { $size: { $setIntersection: [ fav_actors, "$cast" ] } } } },
+  { $match:
+    { cast: { $exists: true },
+      countries: { $eq: "USA" },
+      "tomatoes.viewer.rating": { $gte: 3 } } },
+  { $addFields: { num_favs: { $size: { $setIntersection: [favActors, "$cast"] } } } },
   { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
   { $project: { _id: 0, title: 1 } },
   { $skip: 24 },
