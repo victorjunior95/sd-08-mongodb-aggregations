300633522dffa2099eed2ab06813c1809dd5917a
Leonardo <lecastro16@gmail.com>
2021-05-14 09:51:40 -0300

    requisito 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,2 @@
+db.trips.aggregate([{$match: {$and: [{birthYear: {$not: {$eq: ""}}},{birthYear: {$exists: true}}]}},{$group: {_id: null, maiorAnoNascimento: {$max: {$toInt: "$birthYear"}}, menorAnoNascimento: {$min: {$toInt:"$birthYear"}}}},{$project: {_id: 0, maiorAnoNascimento: 1, menorAnoNascimento: 1}}])
+
