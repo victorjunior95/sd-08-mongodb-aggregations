f448b2596ff13a8086830a90cbeecb286660b94c
Giovanna Betti <giovannabetti@gmail.com>
2021-05-14 18:22:36 -0300

    Challenge 11 done

---
+++ challenges/desafio11.js
@@ -0,0 +1,32 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      diaDaSemana: {
+        $dayOfWeek: "$startTime",
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$diaDaSemana",
+      count: {
+        $sum: 1,
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$count",
+    },
+  },
+  {
+    $sort: {
+      total: -1,
+    },
+  },
+  {
+    $limit: 1,
+  },
+]);
