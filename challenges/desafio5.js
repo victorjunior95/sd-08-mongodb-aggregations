04239240ce404a0ad8e1e9377aafb32a2038cf68
Uilton Rocha <itoclick@gmail.com>
2021-05-14 09:38:56 -0300

    desafio 6

---
+++ challenges/desafio5.js
@@ -0,0 +1,22 @@
+const lista = [
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney",
+];
+db.movies.aggregate([
+  {
+    $match: {
+      countries: "USA" ,
+      "tomatoes.viewer.rating": { $gte: 3 } ,
+      cast:{$exists:true},
+      
+    },
+  },
+  { $project: { num_favs: {$size: {$setIntersection: ["$cast", lista] }}, title: 1, "tomatoes.viewer.rating": 1, _id:0 } },
+  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
+  {$skip:24},
+  {$limit:1},
+  {$project:{ title: 1}}
+]);
