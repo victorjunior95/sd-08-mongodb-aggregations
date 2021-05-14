00285f38e81ba65b9c0522fd38675490e0385365
Uilton Rocha <itoclick@gmail.com>
2021-05-14 12:30:40 -0300

    desafio 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,8 +1,6 @@
-db.movies.createIndex({ awards: "text" });
-
 db.movies.aggregate([
   {
-    $match: { $text: { $search: "won" }, "imdb.rating": [{ $ne: "" }, { $lte: 9.2 }] },
+    $match: { awards: { $regex: /^won\s\d\soscar/i } },
   },
   {
     $group:
@@ -21,7 +19,7 @@ db.movies.aggregate([
       maior_rating: 1,
       menor_rating: 1,
       media_rating: { $round: ["$media", 1] },
-      desvio_padrao: { $trunc: ["$desvio", 1] },
+      desvio_padrao: { $round: ["$desvio", 1] },
 
     },
   },
