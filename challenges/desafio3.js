88516fa4c5440ae62c9c36e808c14c0d45657b82
Lucas Neiva <lucassneiva.code@gmail.com>
2021-05-14 22:23:05 -0300

    fix desafio3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -5,5 +5,5 @@ db.movies.aggregate([
     languages: { $all: ["English", "Spanish"] } },
   },
   { $project: { _id: 0, titulo: "$title", avaliado: "$rated", notaIMDB: "$imdb.rating", votosIMDB: "$imdb.votes", ano: "$year" } },
-  { $sort: { titulo: 1, year: -1, notaIMDB: -1 } },
+  { $sort: { ano: -1, notaIMDB: -1, titulo: 1 } },
 ]);
