d99a0afaff8c1b501c8f299be16aafe963c9873a
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-05-15 16:35:14 -0300

    start: challenge 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,33 @@
+db.movies.aggregate([
+  {
+    $match: {
+      languages: "English",
+    },
+  },
+  {
+    $unwind: "$cast",
+  },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaIMDB: { $avg: "$imdb.rating" },
+    },
+  },
+  {
+    $project: {
+      _id: 1,
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$mediaIMDB", 1] },
+    },
+  },
+  {
+    $sort: {
+      numeroFilmes: -1,
+      _id: 1,
+    },
+  },
+  {
+    $limit: 50000,
+  },
+]);
