c69cd4bb2dc49be1640ce4535bf8468a707daab1
Uilton Rocha <itoclick@gmail.com>
2021-05-15 02:22:19 -0300

    desafio 7

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,26 +1,13 @@
 db.movies.aggregate([
-  { $match: {
-    languages: ["English"],
-    "imdb.rating": { $ne: "" } },
-  },
-  {
-    $unwind: "$cast",
-  },
-  {
-    $group: {
-      _id: null,
-      total: { $sum: 1 },
-    //  media: { $avg: "$imdb.rating" },
-    },
-  },
-  /* {
-    $project: {
-      _id: 1,
-      numeroFilmes: { $round: ["$total", 1] },
-      mediaIMDB: { $round: ["$media", 1] },
 
-    },
-  },
+  { $match: { languages: "English" } },
+
+  { $unwind: "$cast" },
+
+  { $group: { _id: "$cast", total: { $sum: 1 }, media: { $avg: "$imdb.rating" } } },
+
+  { $project: { _id: 1, numeroFilmes: { $round: ["$total", 1] }, mediaIMDB: { $round: ["$media", 1] } } },
+
   { $sort: { numeroFilmes: -1, mediaIMDB: -1 } },
- */
+
 ]);
