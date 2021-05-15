e9c98f2b3b810070d1ef7f38e49fe3056e6acaab
albertassi88 <albertassi88@gmail.com>
2021-05-15 13:07:19 -0300

    Desafio 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,1 @@
+db.trips.aggregate([{ $addFields: { dia: { $dayOfWeek: "$startTime" } } }, { $match: { dia: 5 } }, { $group: { _id: "$startStationName", total: { $sum: 1 } } }, { $sort: { total: -1 } }, { $limit: 1 }, { $project: { nomeEstacao: "$_id", total: "$total", _id: 0 } }]);
