171fcd35d2a8adeb2328391fe8ce688ee211bdeb
albertassi88 <albertassi88@gmail.com>
2021-05-15 11:26:08 -0300

    Alteracoes no desafio 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,6 +1,1 @@
-db.trips.aggregate([{ $group: { _id: usertype, oi: { $sum: 1 },
-} },
-{
-  $project: {
-    duracaoMedia: { $subtract: ["$startTime", "$stopTime"] } } },
-]);
+db.trips.aggregate([{ $group: { _id: "$usertype", media: { $avg: { $subtract: ["$stopTime", "$startTime"] } } } }, { $project: { _id: 0, tipo: "$_id", duracaoMedia: { $round: [{ $divide: ["$media", 60 * 60 * 1000] }, 2] } } }, { $sort: { duracaoMedia: 1 } }]);
