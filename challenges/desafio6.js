e99ad0313d472ccac774a2839a0687eb76731b14
Felipe Segovia <felipe.vsegovia@gmail.com>
2021-05-15 14:25:30 -0300

    MongoDB - Aggregations - Req06

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,21 +1,30 @@
 db.movies.aggregate([
   {
-    $match:
+    $match: // Regex feito e entendido com a ajuda do mestre Paulo Simões
     {
-      awards: { $exists: true, $ne: " " },
+      awards: { $regex: /^Won \d+ oscar/i },
     },
-
   },
   {
     $group:
     {
-      _id: "$imdb.rating",
-      totalRating: { $sum: 1 },
+      _id: null,
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
     },
   },
   {
     $project: {
-      maior_rating: { _id: [{ $sort: { _id: -1 }, $skip: 1 }] },
+      _id: 0,
+      maior_rating: "$maior_rating",
+      menor_rating: "$menor_rating",
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
     },
+  },
+  {
+    $limit: 1,
   },
 ]);
