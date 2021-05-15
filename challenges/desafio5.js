3124e79a7d07faa087944c557441914e2406e2d8
Clênio Galvão <clenio.cgm@gmail.com>
2021-05-15 17:30:01 -0300

    Resolve quesito 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,17 @@
+db.movies.aggregate([
+  {
+    $match: {
+      countries: "USA",
+      "tomatoes.viewer.rating": { $gte: 3 },
+      cast: { $exists: true },
+    },
+  },
+  {
+    $addFields: { fav_acts: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"] }
+  },
+  { $addFields: { num_favs: { $size: { $setIntersection: ["$fav_acts", "$cast"] } } } },
+  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
+  { $project: { _id: 0, title: 1 } },
+  { $skip: 24 },
+  { $limit: 1 },
+]);
