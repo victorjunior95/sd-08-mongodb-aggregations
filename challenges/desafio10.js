639d440e168857441edfc4e7487145007de3a5df
Lucas Neiva <lucassneiva.code@gmail.com>
2021-05-16 18:17:38 -0300

    desafio10 done

---
+++ challenges/desafio10.js
@@ -0,0 +1,14 @@
+const HOUR = 60 * 60 * 1000;
+db.trips.aggregate([
+  { $group: {
+    _id: "$usertype",
+    duracaoMedia:
+    { $avg: {
+      $divide: [ { $subtract: ["$stopTime", "$startTime"] }, HOUR ] },
+  } } },
+  { $project: {
+    _id: 0,
+    tipo: "$_id", 
+    duracaoMedia: { $round: ["$duracaoMedia", 2] },
+  } },
+]);
