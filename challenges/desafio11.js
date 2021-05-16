cf733be8b3ae3eb7495ac247c4ff8cbec1d949bf
Lucas Neiva <lucassneiva.code@gmail.com>
2021-05-16 17:11:25 -0300

    fix lint - 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,11 +1,11 @@
 db.trips.aggregate([
   { $group: {
-      _id: { $dayOfWeek: "$startTime" }, total: { $sum: 1 },
+    _id: { $dayOfWeek: "$startTime" }, total: { $sum: 1 },
   } },
   { $project: {
-      _id: 0,
-      diaDaSemana: "$_id",
-      total: "$total",
+    _id: 0,
+    diaDaSemana: "$_id",
+    total: "$total",
   } },
   { $sort: { total: -1 } },
   { $limit: 1 },
