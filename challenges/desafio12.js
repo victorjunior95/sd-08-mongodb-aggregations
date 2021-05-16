a254dc1ebb9b2feaafebad19f70acb8c5e3cd851
MEianelli <filipelouzano@gmail.com>
2021-05-16 20:02:07 -0300

    Requisito All

---
+++ challenges/desafio12.js
@@ -0,0 +1,59 @@
+db.trips.aggregate(
+  {
+    $addFields: {
+      diaDaSemana: {
+        $dayOfWeek: "$startTime",
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$diaDaSemana",
+      count: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$count",
+    },
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+  {
+    $lookup: {
+      from: "trips",
+      let: { dia: "$diaDaSemana" },
+      pipeline: [
+        {
+          $match: {
+            $expr: {
+              $eq: [
+                "$$dia",
+                { $dayOfWeek: "$startTime" },
+              ],
+            },
+          },
+        },
+        {
+          $group: {
+            _id: "$startStationName",
+            total: { $sum: 1 },
+          },
+        },
+      ],
+      as: "estacoes",
+    },
+  },
+  { $unwind: "$estacoes" },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$estacoes._id",
+      total: "$estacoes.total",
+    },
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+);
