82d15ed74c998ed2084e597e92cae38d23ab8699
Clênio Galvão <clenio.cgm@gmail.com>
2021-05-16 18:02:20 -0300

    Resolve quesito 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,11 @@
+db.trips.aggregate([
+  { $project: { _id: 0, diaDaSemana: { $dayOfWeek: { date: "$startTime" } } } },
+  { $group: { _id: "$diaDaSemana", total: { $sum: 1 } } },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+  { $project: {
+    _id: 0,
+    diaDaSemana: "$_id",
+    total: "$total",
+  } },
+]);
