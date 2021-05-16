1688cc3e8911d28266006343a5c568137a5cea4c
Joao <joaocarlitoneto@gmail.com>
2021-05-16 16:15:12 -0300

    Req 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,45 @@
+// Usando a pipeline anterior que retorna o dia com mais viagens,
+// determine qual estação tem o maior número de viagens nesse dia da semana.
+// Exiba apenas o nome da estação e o total de viagens.
+// Dica: Utilize o operador $dayOfWeek para extrair o dia da semana como um número de uma data.
+
+// O resultado da sua query deve ter o seguinte formato:
+
+// { "nomeEstacao" : <nome_da_estacao>, "total" : <total_de_viagens> }
+
+db.trips.aggregate(
+  [
+    {
+      $match: {
+        $expr: {
+          $eq: [{
+            $dayOfWeek: "$startTime",
+          }, 5],
+        },
+      },
+    },
+    {
+      $group: {
+        _id: "$startStationName",
+        total: {
+          $sum: 1,
+        },
+      },
+    },
+    {
+      $project: {
+        nomeEstacao: "$_id",
+        total: "$total",
+        _id: 0,
+      },
+    },
+    {
+      $sort: {
+        total: -1,
+      },
+    },
+    {
+      $limit: 1,
+    },
+  ],
+);
