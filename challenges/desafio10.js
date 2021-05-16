a9245d6acf8cfadeca180d72a310fd685eee77ea
Uilton Rocha <itoclick@gmail.com>
2021-05-16 19:31:55 -0300

    desafio 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,5 +1,9 @@
+const umahora = 3600000;
 db.trips.aggregate([
   { $group: { _id: "$usertype", media: { $avg: { $subtract: ["$stopTime", "$startTime"] } } } },
-  { $project: { tipo: "_id", duracaoMedia: { $round: [{ $divide: ["$media", 3600000] }, 2] } } },
-
+  { $project: {
+    tipo: "_id",
+    duracaoMedia: { $round: [{ $divide: ["$media", umahora] }, 2] },
+    _id: 0 },
+  },
 ]);
