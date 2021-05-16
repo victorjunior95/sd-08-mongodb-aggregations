c8b83ad6083cccd61673e220cf0d9f5e843968b5
Joao <joaocarlitoneto@gmail.com>
2021-05-16 15:42:27 -0300

    Req 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -4,3 +4,33 @@
 // O resultado da sua query deve ter o seguinte formato:
 
 // { "diaDaSemana" : <dia_da_semana>, "total" : <total_de_viagens> }
+
+db.trips.aggregate(
+  [
+    {
+      $group: {
+        _id: {
+          $dayOfWeek: "$startTime",
+        },
+        total: {
+          $sum: 1,
+        },
+      },
+    },
+    {
+      $project: {
+        diaDaSemana: "$_id",
+        total: "$total",
+        _id: 0,
+      },
+    },
+    {
+      $sort: {
+        total: -1,
+      },
+    },
+    {
+      $limit: 1,
+    },
+  ],
+);
