49f1f4c6f5d617ad1865930507d6efd9038179b0
Uilton Rocha <itoclick@gmail.com>
2021-05-17 10:40:49 -0300

    desafio 14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,1 +1,15 @@
-console.log(ok);
+db.trips.aggregate([
+
+  {
+    $group:
+      {
+        _id: "$bikeid",
+        tempoMedio: { $avg: { $subtract: ["$stopTime", "$startTime"] } },
+      },
+
+  },
+
+  { $project: { duracaoMedia: { $ceil: { $divide: ["$tempoMedio", 60000] } }, bikeId: "$_id", _id: 0 } },
+  { $sort: { duracaoMedia: -1 } },
+  { $limit: 5 },
+]);
