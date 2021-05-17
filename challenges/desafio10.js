4439732832559b2c3aaf18353d790db54a561be1
Uilton Rocha <itoclick@gmail.com>
2021-05-17 10:07:35 -0300

    desafio 10 refactoring

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,6 +1,9 @@
 const umahora = 3600000;
 db.trips.aggregate([
-  { $group: { _id: "$usertype", media: { $avg: { $subtract: ["$stopTime", "$startTime"] } } } },
+  { $group: {
+    _id: "$usertype",
+    media: { $avg: { $subtract: ["$stopTime", "$startTime"] } } } },
+
   { $project: {
     tipo: "$_id",
     duracaoMedia: { $round: [{ $divide: ["$media", umahora] }, 2] },
