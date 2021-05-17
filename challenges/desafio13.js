b3431334b6f92e17827a6ac78cdfdf40dcfd8ba7
jnpefo <jnpefo@gmail.com>
2021-05-17 13:38:27 -0300

    Req 13 e 14

---
+++ challenges/desafio13.js
@@ -0,0 +1,35 @@
+db.trips.aggregate([
+  {
+    $match: {
+      $and: [
+        {
+          startTime: { $gt: ISODate("2016-03-10") },
+        },
+        {
+          startTime: { $lt: ISODate("2016-03-11") },
+        },
+      ],
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      media: {
+        $avg: {
+          $divide: [
+            { $subtract: ["$stopTime", "$startTime"] },
+            60000,
+          ],
+        },
+      },
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      duracaoMediaEmMinutos: {
+        $ceil: "$media",
+      },
+    },
+  },
+]);
---
+++ challenges/desafio14.js
@@ -0,0 +1,28 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$bikeid",
+      media: {
+        $avg: {
+          $divide: [
+            { $subtract: ["$stopTime", "$startTime"] },
+            60000,
+          ],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      bikeId: "$_id",
+      duracaoMedia: { $ceil: "$media" },
+    },
+  },
+  {
+    $sort: { duracaoMedia: -1 },
+  },
+  {
+    $limit: 5,
+  },
+]);
