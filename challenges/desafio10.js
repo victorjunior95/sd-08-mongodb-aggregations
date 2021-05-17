15a368bc30a85bfcbc79b5e80aebb36e71cdbd24
MEianelli <filipelouzano@gmail.com>
2021-05-17 14:41:27 -0300

    Problemas arrumados

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,14 +1,15 @@
-const HORAS = 1000 * 60 * 60;
+const HOUR = 60 * 60 * 1000;
 db.trips.aggregate(
   { $addFields: {
     duracao: {
       $divide: [
         { $subtract: ["$stopTime", "$startTime"] },
-        HORAS,
+        HOUR,
       ] } } },
   { $group: {
     _id: "$usertype",
-    duracaoMedia: { $avg: "$duracao" } } }, { $project: {
+    duracaoMedia: { $avg: "$duracao" } } },
+  { $project: {
     _id: 0,
     tipo: "$_id",
     duracaoMedia: { $round: ["$duracaoMedia", 2] } } },
