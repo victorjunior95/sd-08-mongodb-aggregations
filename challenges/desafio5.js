ce49ad17f27c3879969dcba3a2b881af31ccd256
Bianca <bruninhakjurugat11@gmail.com>
2021-05-17 11:10:51 -0300

    linter fix

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,44 +1,44 @@
-const fav_actors = [
-    'Sandra Bullock',
-    'Tom Hanks',
-    'Julia Roberts',
-    'Kevin Spacey',
-    'George Clooney',
+const favActors = [
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney",
 ];
 
 db.movies.aggregate([
-    {
-        $match: {
-            countries: 'USA',
-            'tomatoes.viewer.rating': { $gte: 3 },
-            cast: { $exists: true },
+  {
+    $match: {
+      countries: "USA",
+      "tomatoes.viewer.rating": { $gte: 3 },
+      cast: { $exists: true },
+    },
+  },
+  {
+    $addFields: {
+      num_favs: {
+        $size: {
+          $setIntersection: ["$cast", favActors],
         },
+      },
     },
-    {
-        $addFields: {
-            num_favs: {
-                $size: {
-                    $setIntersection: ['$cast', fav_actors],
-                },
-            },
-        },
+  },
+  {
+    $sort: {
+      num_favs: -1,
+      "tomatoes.viewer.rating": -1,
+      title: -1,
     },
-    {
-        $sort: {
-            num_favs: -1,
-            'tomatoes.viewer.rating': -1,
-            title: -1
-        },
+  },
+  {
+    $project: {
+      _id: 0, title: 1,
     },
-    {
-        $project: {
-            _id: 0, title: 1
-        },
-    },
-    {
-        $skip: 24
-    },
-    {
-        $limit: 1
-    },
+  },
+  {
+    $skip: 24,
+  },
+  {
+    $limit: 1,
+  },
 ]);
