29776f8ddfb419128d7fb485d4e78c9b79a2162d
Arnaelcio <arnaelcio.gomes@gmail.com>
2021-05-18 07:56:34 -0300

    req 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,32 @@
+db.trips.aggregate([
+    {
+      $match: {
+        startTime: {
+          $gte:  new Date("2016-03-10"),
+          $lt:  new Date("2016-03-11"),
+        },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        minutesDuration: {
+          $avg: {
+            $divide: [
+              { $subtract: ["$stopTime", "$startTime"] },
+              60000,
+            ],
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        duracaoMediaEmMinutos: {
+          $ceil: "$minutesDuration",
+        },
+      },
+    },
+  ]);
+  
