db797d47bcfb367e4a367822efde47dbeea2beb6
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-05-18 01:13:07 -0300

    requisito 5 - vigesimo quinto doc

---
+++ challenges/desafio5.js
@@ -0,0 +1,41 @@
+const arr = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
+
+db.movies.aggregate([
+  {
+    $match: {
+      countries: "USA",
+      "tomatoes.viewer.rating": { $gte: 3 },
+      cast: {
+        $in: arr,
+      },
+    },
+  },
+  {
+    $addFields: {
+      num_favs: {
+        $size: {
+          $setIntersection: [arr, "$cast"],
+        },
+      },
+    },
+  },
+  {
+    $sort: {
+      num_favs: -1,
+      "tomatoes.viewer.rating": -1,
+      title: -1,
+    },
+  },
+  {
+    $skip: 24,
+  },
+  {
+    $limit: 1,
+  },
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+    },
+  },
+]);
