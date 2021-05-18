48c5d7235cc3f588171e82b2f08abac67b5a12eb
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-05-18 20:42:20 -0300

    fix req6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,19 +1,13 @@
 db.movies.aggregate([
-  {
-    $match: {
-      awards: {
-        $regex: /won \d oscar/i,
-      },
-    },
-  },
+  { $match: { awards: { $regex: /won \d oscar/i } } },
 
   {
     $group: {
       _id: null,
-      maior_rating: { $max: "$imdb.rating " },
-      menor_rating: { $min: "$imdb.rating " },
-      media_rating: { $avg: "$imdb.rating " },
-      desvio_padrao: { $stdDevSamp: "$imdb.rating " },
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
     },
   },
 
