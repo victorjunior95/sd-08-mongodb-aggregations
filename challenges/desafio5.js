c6576c072eb50ef7f4477ce86280f20d37193703
victorjunior95 <victorjr.muchacho@gmail.com>
2021-05-18 23:09:19 -0300

    Challenge 7 completed

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -25,14 +25,15 @@ const actFav = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", 
 
 db.movies.aggregate([
   { $match: {
-    cast: { $exists: true },
     countries: "USA",
     "tomatoes.viewer.rating": { $gte: 3 },
+    cast: { $exists: true },
   } },
   { $addFields: {
-    num_favs: { $size: { $setIntersection: [actFav, $cast] } },
+    num_favs: { $size: { $setIntersection: [actFav, "$cast"] } },
   } },
   { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
   { $skip: 24 },
   { $limit: 1 },
+  { $project: { _id: 0, title: 1 } },
 ]);
--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -20,6 +20,7 @@
 db.movies.aggregate([
   { $match: { awards: { $regex: /Won [1-9] Oscar/i } } },
   { $group: {
+    _id: null,
     maior_rating: { $max: "imdb.rating" },
     menor_rating: { $min: "imdb.rating" },
     media_rating: { $avg: "imdb.rating" },
