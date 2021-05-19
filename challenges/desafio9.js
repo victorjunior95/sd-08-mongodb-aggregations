d500ee0eca8056f1f81a5b59c26a8bfbd51742d8
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-05-19 10:26:40 -0300

    req9 ok

---
+++ challenges/desafio9.js
@@ -0,0 +1,23 @@
+db.trips.aggregate([
+  {
+    $match: {
+      birthYear: { $gt: 0 },
+    },
+  },
+
+  {
+    $group: {
+      _id: null,
+      maiorAnoNascimento: { $max: { $toInt: "$birthYear" } },
+      menorAnoNascimento: { $min: { $toInt: "$birthYear" } },
+    },
+  },
+
+  {
+    $project: {
+      _id: 0,
+      maiorAnoNascimento: 1,
+      menorAnoNascimento: 1,
+    },
+  },
+]);
