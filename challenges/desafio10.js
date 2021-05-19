16d0c9f22db9a0e6d2a120d8431c7069769c87ad
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-05-19 09:15:14 -0300

    C9 - 10th Req Sent

---
+++ challenges/desafio10.js
@@ -0,0 +1,48 @@
+const horasEmMs = 3600000;
+
+db.trips.aggregate([
+  {
+    $addFields:
+    {
+      duracao:
+      {
+        $divide:
+        [
+          {
+            $subtract:
+            [
+              "$stopTime",
+              "$startTime",
+            ],
+          },
+          horasEmMs,
+        ],
+      },
+    },
+  },
+  {
+    $group:
+    {
+      _id: "$usertype",
+      duracao_media: { $avg: "$duracao" },
+    },
+  },
+  {
+    $project:
+    {
+      _id: 0,
+      tipo: "$_id",
+      duracao_media:
+      {
+        $round:
+        [
+          "$duracao_media",
+          2,
+        ],
+      },
+    },
+  },
+  {
+    $sort: { duracao_media: 1 },
+  },
+]);
