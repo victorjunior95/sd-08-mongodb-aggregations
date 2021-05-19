497df3c98ef4913343449540db8403c13220cd08
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-05-19 11:43:29 -0300

    fix lint req10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -15,16 +15,16 @@ db.trips.aggregate([
       _id: 0,
       tipo: "$_id",
       duracaoMedia: {
-        $round: [{ $divide: ["$duracaoMedia", 3.6e+6] }, 2],
+        $round: [{ $divide: ["$duracaoMedia", 3.6e6] }, 2],
       },
     },
   },
 
-    {
-      $sort: {
-        duracaoMedia: 1,
-      }
-    }
+  {
+    $sort: {
+      duracaoMedia: 1,
+    },
+  },
 ]);
 
 //divisor 3.6e+6 encontrado via Pesquisa Google "transformar milisegundos em horas" in https://www.google.com/search?q=transformar+milisegundos+em+horas&oq=transformar+milise&aqs=chrome.2.0l3j69i57j0l4.4444j0j1&sourceid=chrome&ie=UTF-8
