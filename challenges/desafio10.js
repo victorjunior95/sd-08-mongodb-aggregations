f27a5cdc16c4b54ebdb5aabe0c5b11bfec87c1fb
Blucilha <blucilha.nascimento@gmail.com>
2021-05-19 20:27:45 -0300

    desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,31 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      variacaoTempo: {
+        $subtract: ["$stopTime", "$startTime"],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$usertype",
+      media: {
+        $avg: "$variacaoTempo",
+      },
+    },
+  },
+  {
+    $project: {
+      tipo: "$_id",
+      duracaoMedia: {
+        $round: [
+          {
+            $divide: ["$media", 3600000],
+          },
+          2,
+        ],
+      },
+      _id: 0,
+    },
+  },
+]);
