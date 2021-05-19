dbdc01f25775012614410a091962a06fd8b60ba4
lucasbgama <lucasbg@protonmail.com>
2021-05-19 00:16:40 -0300

    desafios 11 e 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,35 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      diaDaSemana: { $dayOfWeek: "$startTime" },
+    },
+  },
+  {
+    $lookup: {
+      from: "trips",
+      let: { diaDaSemana: "$diaDaSemana" },
+      pipeline: [
+        { $group: { _id: { $dayOfWeek: "$startTime" }, total: { $sum: 1 } } },
+        { $project: { _id: 0, maxDiaDaSemana: "$_id", semanaTotal: "$total" } },
+        { $sort: { semanaTotal: -1 } },
+        { $limit: 1 },
+        { $match: { $expr: { $eq: ["$maxDiaDaSemana", "$$diaDaSemana"] } } },
+      ],
+      as: "maxTrips",
+    },
+  },
+  {
+    $group: {
+      _id: "$startStationName", total: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id",
+      total: "$total",
+    },
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
