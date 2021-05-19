44f98b690f2a7f8f2f6ed284bda7aa8a1d765945
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-05-19 09:22:25 -0300

    C10 - 11th Req Sent

---
+++ challenges/desafio11.js
@@ -0,0 +1,21 @@
+db.trips.aggregate([
+  {
+    $group:
+    {
+      _id: { $dayOfWeek: "$startTime" },
+      startTrips: { $sum: 1 },
+    },
+  },
+  {
+    $project:
+    {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$startTrips",
+    },
+  },
+  {
+    $sort: { total: -1 },
+  },
+  { $limit: 1 },
+]);
