065150ce2f68b6ef4862379164b66c296dde3622
Bianca <bruninhakjurugat11@gmail.com>
2021-05-19 22:11:42 -0300

    desafio10 done

---
+++ challenges/desafio10.js
@@ -0,0 +1,19 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$userType",
+      duracaoMedia: {
+        $avg: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3600000] },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: {
+        $round: ["$duracaoMedia", 2],
+      },
+    },
+  },
+]);
