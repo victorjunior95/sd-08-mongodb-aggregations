68d412f8629932c78b5d5d690202cc9f26be24c6
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-05-21 17:49:09 -0300

    Desafio 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -21,3 +21,24 @@ O resultado da sua query deve ter o seguinte formato:
   "desvio_padrao" : <desvio_padrao>
 }
 */
+db.movies.aggregate([
+  { $match: { awards: { $all: [/.*won.*/i, /.*oscar.*/i] }, "imdb.rating": { $gt: 0 } } },
+  {
+    $group: {
+      _id: 0,
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maior_rating: 1,
+      menor_rating: 1,
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
+    },
+  },
+]);
