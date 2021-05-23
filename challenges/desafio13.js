2129c8df8d5a09d33d6dd84e230d21ff14fc7aba
victorjunior95 <victorjr.muchacho@gmail.com>
2021-05-23 14:30:59 -0300

    Challenge 13 completed

---
+++ challenges/desafio13.js
@@ -0,0 +1,18 @@
+// Desafio 13 - Determine a duração média das viagens iniciadas no dia 10/03/2016, em minutos.
+//  a) Arredonde o resultado para cima.
+// O resultado da sua query deve ter o seguinte formato:
+// { "duracaoMediaEmMinutos" : <duracao_media_em_minutos> }
+const HrToMs = 3600000;
+db.trips.aggregate([
+  { $match: {
+    startTime: { $gte: new Date("2016-03-10"), $lt: new Date("2016-03-11") },
+  } },
+  { $group: {
+    _id: null,
+    duracaoMediaEmMinutos: { $avg: { $subtract: ["$stopTime", "$startTime"] } },
+  } },
+  { $project: {
+    _id: 0,
+    duracaoMediaEmMinutos: { $ceil: { $divide: ["$duracaoMediaEmMinutos", HrToMs] } },
+  } },
+]);
