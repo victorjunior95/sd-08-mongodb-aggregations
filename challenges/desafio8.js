7a6f4b0c46d7ea454046780374d32b83d160680a
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-05-24 12:47:38 -0300

    Desafio 8

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -13,3 +13,34 @@ O resultado da sua query deve ter o seguinte formato:
 
 { "_id" : <nome_da_alianca>, "totalRotas" : <total_de_rotas> }
 */
+db.air_alliances.aggregate([
+  {
+    $lookup: {
+      from: "air_routes",
+      localField: "airlines",
+      foreignField: "airline.name",
+      as: "air_routes",
+    },
+  },
+  {
+    $unwind: "$air_routes",
+  },
+  // { $limit: 1 },
+  {
+    $match: {
+      "air_routes.airplane": { $in: ["747", "380"] },
+    },
+  },
+  {
+    $group: {
+      _id: "$name",
+      totalRotas: { $sum: 1 },
+    },
+  },
+  {
+    $sort: {
+      totalRotas: -1,
+    },
+  },
+  { $limit: 1 },
+]);
