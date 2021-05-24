021a0be32bbd2d356c985fa1a4d487cabe779926
Thais Quintela <thais.s.quintela@gmail.com>
2021-05-24 01:52:43 -0300

    Corrigindo ordenacao

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -3,14 +3,14 @@ db.trips.aggregate(
     {
       $group: {
         _id: { $dayOfWeek: "$startTime" },
-        total: { $sum: 1 },
+        count: { $sum: 1 },
       },
     },
     {
       $project: {
         _id: 0,
         diaDaSemana: "$_id",
-        total: 1,
+        total: "$count",
       },
     },
     {
--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -13,14 +13,14 @@ db.trips.aggregate(
     {
       $group: {
         _id: "$startStationName",
-        total: { $sum: 1 },
+        count: { $sum: 1 },
       },
     },
     {
       $project: {
-        nomeEstacao: "$_id",
         _id: 0,
-        total: 1,
+        nomeEstacao: "$_id",
+        total: "$count",
       },
     },
     {
