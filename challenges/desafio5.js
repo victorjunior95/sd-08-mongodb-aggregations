55b04aa803b96962d08aa6a055571398864c6528
caiomzrocha <caiomzrocha@gmail.com>
2021-05-24 13:29:49 -0300

    [REQ-05]

---
+++ challenges/desafio5.js
@@ -0,0 +1,41 @@
+db.movies.aggregate([
+  {
+    $match: {
+      countries: "USA",
+      "tomatoes.viewer.rating": { $gte: 3 }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+      "tomatoes.viewer.rating": 1,
+      num_favs: {
+        $cond: {
+          if: { $isArray: "$cast" },
+          then: {
+            $size: {
+              $setIntersection: [
+                [
+                  "Sandra Bullock",
+                  "Tom Hanks",
+                  "Julia Roberts",
+                  "Kevin Spacey",
+                  "George Clooney",
+                ],
+                "$cast"
+              ],
+            },
+          },
+          else: 0,
+        },
+      },
+    },
+  },
+  {
+    $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 },
+  },
+  { $skip: 24 },
+  { $limit: 1 },
+  { $project: { title: 1 } },
+]);
