4c81dc9f380475ecc9bd6dbc86af82f00eba25fc
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-05-27 18:11:48 -0300

    alterando o sort de lugar com o project

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -16,16 +16,16 @@ db.movies.aggregate([
     },
   },
   {
+    $sort: {
+      numeroFilmes: -1,
+      _id: -1,
+    },
+  },
+  {
     $project: {
       _id: 1,
       numeroFilmes: 1,
       mediaIMDB: { $round: ["$mediaIMDB", 1] },
-    },
-  },
-  {
-    $sort: {
-      numeroFilmes: -1,
-      _id: -1,
     },
   },
 ]);
