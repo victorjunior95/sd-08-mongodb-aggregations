d1b997ee2c550351055faa940b5800c904a9f69f
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-05-29 23:32:07 -0300

    requisito 8 feito

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,9 +1,38 @@
-db.air_routes.aggregate([
+use("aggregations");
+
+db.air_alliances.aggregate([
+  {
+    $unwind: "$airlines",
+  },
   {
     $lookup: {
-      from: "air_alliances",
-      localField: "airplane",
-      foreignField: "",
+      from: "air_routes",
+      localField: "airlines",
+      foreignField: "airline.name",
+      as: "routes_alliances",
     },
+  },
+  {
+    $match: { "routes_alliances.airplane": { $in: ["747", "380"] } },
+  },
+  {
+    $unwind: "$routes_alliances",
+  },
+  {
+    $match: { "routes_alliances.airplane": { $in: ["747", "380"] } },
+  },
+  {
+    $group: {
+      _id: "$name",
+      totalRotas: { $sum: 1 },
+    },
+  },
+  {
+    $sort: {
+      totalRotas: -1,
+    },
+  },
+  {
+    $limit: 1,
   },
 ]);
