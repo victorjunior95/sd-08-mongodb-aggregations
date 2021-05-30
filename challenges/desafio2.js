48deefa5a9ebafdb2c0b8515b3bfb824eebb961e
rodolfo_oliveira <rod.vgo@gmail.com>
2021-05-30 14:07:23 -0300

    desafio 1 e 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -7,4 +7,14 @@ db.movies.aggregate([
       languages: { $all: ["English", "Spanish"] },
     },
   },
+  {
+    $project: {
+      _id: 0,
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
 ]);
