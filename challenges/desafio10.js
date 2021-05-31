7fd91f1a4f89f310c38bbf7d734d5719edc47d7b
caiomzrocha <caiomzrocha@gmail.com>
2021-05-31 14:27:19 -0300

    [REQ-10] Calcula a média de tempo de viagem de acordo com o tipo de usuário e ordena essa média de forma crescente.

---
+++ challenges/desafio10.js
@@ -0,0 +1,23 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: {
+        $avg: {
+          $divide: [
+            { $subtract: ["$stopTime", "$startTime"] },
+            1000 * 60 * 60,
+          ],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      duracaoMedia: { $round: ["$duracaoMedia", 2] },
+    },
+  },
+  {
+    $sort: { duracaoMedia: 1 },
+  },
+]);
