422c35c3355bf7200c2b3df8cf2201572bd84651
Rafael Greco <rafaelmgreco@hotmail.com>
2021-06-09 11:15:23 -0300

    req 5 6 7 8 9 10

---
+++ challenges/desafio8.js
@@ -0,0 +1,13 @@
+db.air_alliances.aggregate([
+  { $unwind: "$airlines" },
+  { $lookup: {
+    from: "air_routes",
+    localField: "airlines",
+    foreignField: "airline.name",
+    as: "details",
+  } },
+  { $unwind: "$details" },
+  { $match: { "details.airplane": { $in: ["747", "380"] } } },
+  { $group: { _id: "$name", totalRotas: { $sum: 1 } } },
+  { $sort: { totalRotas: -1 } }, { $limit: 1 },
+]);
