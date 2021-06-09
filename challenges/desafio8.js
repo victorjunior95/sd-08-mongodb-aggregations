5eef4af599882b90a131960ad30c987687470170
enicio <jessandro42@gmail.com>
2021-06-09 12:12:29 -0300

    requisito 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,19 @@
+db.air_alliances.aggregate([{
+  $lookup: {
+    from: "air_routes",
+    localField: "airlines",
+    foreignField: "airline.name",
+    as: "alliance_routes"
+  },
+},
+{ $unwind: "$alliance_routes" },
+{ $match: {
+  "alliance_routes.airplane": { $in: ["747", "380"] },
+},
+},
+{ $group: {
+  _id: "$name", totalRotas: { $sum: 1 },
+},
+},
+{ $limit: 1 },
+]);
