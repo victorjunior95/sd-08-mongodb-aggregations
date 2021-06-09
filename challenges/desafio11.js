c67daa9f39cf8f7f0bad824a5af97a447ab7c87a
Rafael Greco <rafaelmgreco@hotmail.com>
2021-06-09 11:34:16 -0300

    req 11 12 13

---
+++ challenges/desafio11.js
@@ -0,0 +1,5 @@
+db.trips.aggregate([
+  { $group: { _id: { $dayOfWeek: "$startTime" }, count: { $sum: 1 } } },
+  { $project: { diaDaSemana: "$_id", total: "$count", _id: 0 } },
+  { $sort: { total: -1 } }, { $limit: 1 },
+]);
