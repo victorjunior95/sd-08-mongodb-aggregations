116d3138be9ac9f4ae5a1f42f2e901460bf0d72a
Andre Sampaio <andresampaiolm@gmail.com>
2021-06-10 00:56:36 -0300

    REQ 01

---
+++ challenges/desafio1.js
@@ -0,0 +1,18 @@
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        { "imdb.rating": { $gte: 7 } },
+        { genres: { $nin: ["Crime", "Horror"] } },
+        { rated: { $in: ["PG", "G"] } },
+        { languages: { $all: ["English", "Spanish"] } },
+      ],
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      count: { $sum: 1 },
+    },
+  },
+]);
