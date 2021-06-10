f99fc2538593db95e5ec7ce5fb72f8540bbff809
Isabella Paz <bellapaz.iorp@gmail.com>
2021-06-10 02:36:19 -0300

    desafio 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,20 @@
+// Determine qual o dia da semana com maior número de viagens iniciadas
+// Dica: Utilize $dayOfWeek para extrair o dia da semana como um número de uma data
+
+db.trips.aggregate([
+  {
+    $group: {
+      _id: { $dayOfWeek: "$startTime" },
+      total: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$total",
+    },
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
