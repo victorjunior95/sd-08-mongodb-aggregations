34a0af452451e535bf3c09b57dbf1df8e8177e1c
Victor Moraes <moraesvictorgk@gmail.com>
2021-06-10 00:12:01 -0300

    challenges/1to10

---
+++ challenges/desafio8.js
@@ -0,0 +1,39 @@
+db.air_alliances.aggregate([
+  {
+    $unwind: "$airlines",
+  },
+  {
+    $lookup: {
+
+      from: "air_routes",
+      localField: "airlines",
+      foreignField: "airline.name",
+      as: "airline_details",
+
+    },
+  },
+  {
+
+    $unwind: "$airline_details",
+
+  },
+  {
+    $match: {
+      "airline_details.airplane": { $in: ["747", "380"] },
+    },
+  },
+  {
+    $group: {
+      _id: "$name",
+      totalRotas: { $sum: 1 },
+    },
+  },
+  {
+    $sort: {
+      totalRotas: -1,
+    },
+  },
+  {
+    $limit: 1,
+  },
+]);
